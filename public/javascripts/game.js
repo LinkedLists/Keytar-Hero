8526e4b05bcae9e8ca15633abc3e285d863ecc4f
appkenny <kzeng.95@gmail.com>
2021-03-03 12:09:47 -0800

    can now hit 2 notes simultaneously

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -2,18 +2,25 @@ import Note from './note'
 import { drawTargets } from './target'
 // import { scoreboard } from './score'
 
+const CONSTANTS = {
+  pos1: 30,
+  pos2: 150,
+  pos3: 270,
+  pos4: 390,
+  pos5: 510,
+}
+
 export default class Game {
   constructor(canvas) {
     this.c = canvas.getContext('2d');
     this.dimensions = { width: canvas.width, height: canvas.height};
     this.score = 0;
 
-    this.notes = []
-    this.note1 = new Note(30, 0, this.c)
-    this.note2 = new Note(390, 0, this.c)
-    this.notes.push(this.note1)
-    this.notes.push(this.note2)
-    
+    this.notes = this.generateNoteArray();
+    this.note1 = new Note(CONSTANTS.pos1, 0, this.c)
+    this.note2 = new Note(CONSTANTS.pos4, 0, this.c)
+    this.notes[0].push(this.note1)
+    this.notes[3].push(this.note2)
     
     this.addListeners = this.addListeners.bind(this)
     this.addListeners()
@@ -28,16 +35,28 @@ export default class Game {
     this.scoreboard()
     drawTargets(this.c);
 
-    this.notes.forEach( note => {
-      note.update();
+    // this.notes.forEach( note => {
+    //   note.update();
+    // })
+    this.notes.forEach( subArr => {
+      // if (subArr.length > 0) {
+        subArr.forEach( note => {
+          note.update()
+        })
+        if (subArr[0] !== undefined && subArr[0].outOfBounds(800)) {
+          console.log("note is unshifted");
+          console.log(subArr[0])
+          subArr.shift();
+        }
+      // }
     })
 
-    if (this.notes[0]) {    
-      if (this.notes[0].outOfBounds(this.dimensions.height)) {
-        console.log("note is unshifted");
-        this.notes.shift();
-      }
-    }
+    // if (this.notes[0]) {    
+    //   if (this.notes[0].outOfBounds(this.dimensions.height)) {
+    //     console.log("note is unshifted");
+    //     this.notes.shift();
+    //   }
+    // }
 
     // this.notes.forEach( note => {
     //   if (note) {    
@@ -53,32 +72,41 @@ export default class Game {
 
   checkCollision(x) {
     if (
-      this.notes[0] && 
-      this.notes[0].x === x) {
-        if (this.notes[0].inBounds(this.dimensions.height)) {
+      this.notes[x][0]) 
+      {
+        if (this.notes[x][0].inBounds(this.dimensions.height)) {
           console.log("hit")
           this.score += 1;
-          this.notes.shift();
+          this.notes[x].shift();
         }
     }
+    // if (
+    //   this.notes[0] && 
+    //   this.notes[0].x === x) {
+    //     if (this.notes[0].inBounds(this.dimensions.height)) {
+    //       console.log("hit")
+    //       this.score += 1;
+    //       this.notes.shift();
+    //     }
+    // }
   }
 
   addListeners() {
     addEventListener('keydown', e => {
       if (e.key == "1") {
-        this.checkCollision(30)
+        this.checkCollision(0)
       } 
       if (e.key == "2") {
-        this.checkCollision(150)
+        this.checkCollision(1)
       } 
       if (e.key == "3") {
-        this.checkCollision(270)
+        this.checkCollision(2)
       } 
       if (e.key == "4") {
-        this.checkCollision(390)
+        this.checkCollision(3)
       } 
       if (e.key == "5") {
-        this.checkCollision(510)
+        this.checkCollision(4)
       } 
     })
   }
@@ -92,5 +120,13 @@ export default class Game {
     this.c.lineWidth = 2;
     this.c.strokeText(this.score, x, y);
     this.c.stroke();
+  }
+
+  generateNoteArray() {
+    let notes = new Array(5);
+    for (let i = 0; i < notes.length; i++) {
+      notes[i] = new Array();
+    }
+    return notes
   }
 } 
