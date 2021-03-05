a519e658339a72682e117b75299b83160e46a1b6
appkenny <kzeng.95@gmail.com>
2021-03-05 10:37:47 -0800

    working background

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,29 +1,11 @@
-// const axios = require('axios');
-
 import Game from "./game"
 // import Game from Board.js
 
 document.addEventListener('DOMContentLoaded', () => {
-
-    // let isbn = '0201558025';
-    // axios.get(`/books/${isbn}`)
-    // .then((response) => {
-    //     console.log(response); 
-    // })
-    // .catch(function (error) {
-    //     console.log(error);
-    // });
-
-    // let query = "grace hopper";
-    // axios.get(`/search?string=${query}`)
-    // .then((response) => {
-    //     console.log(response);
-    // })
-    // .catch(function (error) {
-    //     console.log(error);
-    // });
-
     const canvas = document.getElementById('canvas');
+    canvas.style.background = "url('../assets/g.png') no-repeat center";
+    canvas.style.backgroundSize = "cover";
+    canvas.style.backgroundSize = "cover";
     // canvas.width = window.innerWidth;
     // canvas.height = window.innerHeight;
     new Game(canvas);
