91e922debaff8e61dc49bbfd44425966dcc34e62
appkenny <kzeng.95@gmail.com>
2021-03-06 18:01:22 -0800

    fixed extra beat in verse 2

--- public/bundle.js
+++ public/bundle.js
@@ -856,6 +856,7 @@ const song = [
   { x: CONSTANTS.pos4, y: 0, pos: 3, tempo: 2, hold: 0, chain: true },
   { x: CONSTANTS.pos3, y: 0, pos: 2, tempo: 2, hold: 0, chain: true },
   { x: CONSTANTS.pos4, y: 0, pos: 3, tempo: 2, hold: 0, chain: true },
+  { kill: true },
 
   { x: CONSTANTS.pos1, y: 0, pos: 0, tempo: 1, hold: 2, chain: true },
   { x: CONSTANTS.pos2, y: 0, pos: 1, tempo: 1, hold: 2, chain: true },
