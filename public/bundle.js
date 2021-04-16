d0d13da3f7534ce23ea29323a3c4c69694b73271
appkenny <kzeng.95@gmail.com>
2021-04-16 10:11:35 -0700

    added song selection wheel

--- public/bundle.js
+++ public/bundle.js
@@ -75,19 +75,30 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    let startBtn = document.getElementById('start-btn')
-    startBtn.addEventListener('click', () => {
-        document.getElementsByClassName('game-view')[0].classList.remove('hidden')
-        document.getElementsByClassName('main-menu-container')[0].classList.add('hidden')
-    })
+  let startBtn = document.getElementById('start-btn')
+  let selectBtn = document.getElementById('selection-back-btn')
+
+  let selectCircle = document.getElementsByClassName('song-selection-container-closed')[0]
+  let mainMenu = document.getElementsByClassName('main-menu-container')[0]
+
+  startBtn.addEventListener('click', () => {
+    selectCircle.classList.add('song-selection-container-open')
+    selectCircle.classList.remove('song-selection-container-closed')
+    selectCircle.classList.remove('hidden')
+    mainMenu.classList.add('hidden')
+  })
+
+  selectBtn.addEventListener('click', () => {
+    selectCircle.classList.remove('song-selection-container-open')
+    selectCircle.classList.add('song-selection-container-closed')
+    mainMenu.classList.remove('hidden')
+  })
 
 
-
-    const canvas = document.getElementById('canvas');
-    canvas.style.backgroundSize = "100% 100%";
-    new __WEBPACK_IMPORTED_MODULE_0__game__["a" /* default */](canvas);
-
-    Object(__WEBPACK_IMPORTED_MODULE_1__modal__["a" /* modalHandler */])()
+  const canvas = document.getElementById('canvas');
+  canvas.style.backgroundSize = "100% 100%";
+  new __WEBPACK_IMPORTED_MODULE_0__game__["a" /* default */](canvas);
+  Object(__WEBPACK_IMPORTED_MODULE_1__modal__["a" /* modalHandler */])()
 })
 
 
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,17 +2,28 @@ import Game from './game';
 import { modalHandler } from './modal';
 
 document.addEventListener('DOMContentLoaded', () => {
-    let startBtn = document.getElementById('start-btn')
-    startBtn.addEventListener('click', () => {
-        document.getElementsByClassName('game-view')[0].classList.remove('hidden')
-        document.getElementsByClassName('main-menu-container')[0].classList.add('hidden')
-    })
+  let startBtn = document.getElementById('start-btn')
+  let selectBtn = document.getElementById('selection-back-btn')
+
+  let selectCircle = document.getElementsByClassName('song-selection-container-closed')[0]
+  let mainMenu = document.getElementsByClassName('main-menu-container')[0]
+
+  startBtn.addEventListener('click', () => {
+    selectCircle.classList.add('song-selection-container-open')
+    selectCircle.classList.remove('song-selection-container-closed')
+    selectCircle.classList.remove('hidden')
+    mainMenu.classList.add('hidden')
+  })
+
+  selectBtn.addEventListener('click', () => {
+    selectCircle.classList.remove('song-selection-container-open')
+    selectCircle.classList.add('song-selection-container-closed')
+    mainMenu.classList.remove('hidden')
+  })
 
 
-
-    const canvas = document.getElementById('canvas');
-    canvas.style.backgroundSize = "100% 100%";
-    new Game(canvas);
-
-    modalHandler()
+  const canvas = document.getElementById('canvas');
+  canvas.style.backgroundSize = "100% 100%";
+  new Game(canvas);
+  modalHandler()
 })
