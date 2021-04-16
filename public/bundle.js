78839936cfc077743882f7722372d25e4161dcf0
appkenny <kzeng.95@gmail.com>
2021-04-16 12:45:25 -0700

    carousel items now render properly in a circle

--- public/bundle.js
+++ public/bundle.js
@@ -86,6 +86,9 @@ document.addEventListener('DOMContentLoaded', () => {
     selectCircle.classList.remove('song-selection-container-closed')
     selectCircle.classList.remove('hidden')
     mainMenu.classList.add('hidden')
+
+    setCarouselPositions()
+
   })
 
   selectBtn.addEventListener('click', () => {
@@ -94,16 +97,24 @@ document.addEventListener('DOMContentLoaded', () => {
     mainMenu.classList.remove('hidden')
   })
 
-  let songCarouselWheelItems = document.querySelectorAll('.song-carousel-item')
-  let centerx = parseFloat(getComputedStyle(songCarouselWheelItems[0]).left) 
-  let centery = parseFloat(getComputedStyle(songCarouselWheelItems[0]).top) 
 
-  let theta = (Math.PI / 180) * (360 / songCarouselWheelItems.length)
+  function setCarouselPositions() {
+    let songCarouselWheelItems = document.querySelectorAll('.song-carousel-item')
+    let centerx = parseFloat(getComputedStyle(songCarouselWheelItems[0]).left) 
+    let centery = parseFloat(getComputedStyle(songCarouselWheelItems[0]).top) 
 
-  songCarouselWheelItems.forEach( songItem => {
-      songItem.style.left = `${centerx + 100 * Math.cos(theta)}px`
-      songItem.style.top = `${centery + 100 * Math.sin(theta)}px`
-  })
+    let theta = (Math.PI / 180.0) * (360 / songCarouselWheelItems.length)
+    songCarouselWheelItems[0].style.left = `${centerx + 100 * Math.cos(theta)}px`
+    songCarouselWheelItems[0].style.top = `${centery - 100 * Math.sin(theta)}px`
+  
+    songCarouselWheelItems.forEach( (songItem, i) => {
+        songItem.style.left = `${centerx + 100 * Math.cos(theta * (i + 1))}px`
+        songItem.style.top = `${centery - 100 * Math.sin(theta * (i + 1))}px`
+    })
+    // let theta = (Math.PI / 4.0)
+    // songCarouselWheelItems[0].style.left = `${centerx + 100 * Math.cos(theta)}px`
+    // songCarouselWheelItems[0].style.top = `${centery - 100 * Math.sin(theta)}px`
+  }
 
 
   const canvas = document.getElementById('canvas');
